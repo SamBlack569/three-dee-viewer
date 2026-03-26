72d44562186790a4173f041386622ae23f037884
Samuel Maciel <samuel.maciel@ndbim.eu>
2026-03-26 12:35:28 +0000

    feat: load .frag files

--- src/components/LoadIfcButton.tsx
+++ src/components/LoadIfcButton.tsx
@@ -27,16 +27,43 @@ export const loadModelBtnTemplate: BUI.StatefullComponent<LoadModelBtnState> = (
       const bytes = new Uint8Array(buffer);
       const ifcLoader = components.get(OBC.IfcLoader);
       await ifcLoader.load(bytes, true, file.name.replace('.ifc', ''));
+      BUI.ContextMenu.removeMenus();
+    });
+
+    input.click();
+  };
+
+  const onLoadFrag = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.multiple = false;
+    input.accept = '.frag';
+
+    input.addEventListener('change', async () => {
+      const file = input.files?.[0];
+      if (!file) return;
+      const buffer = await file.arrayBuffer();
+
+      const fragments = components.get(OBC.FragmentsManager);
+      fragments.core.load(buffer, {
+        modelId: file.name.replace('.frag', '')
+      });
+      BUI.ContextMenu.removeMenus();
     });
 
     input.click();
   };
 
   return BUI.html`<bim-button
-    icon="solar:bookmark-square-minimalistic-bold"
-    label="Load IFC"
-    @click=${onLoadIfc}
-  ></bim-button>`;
+    style="width: 100%;"
+    icon="solar:add-circle-bold"
+    label="Load model"
+  >
+    <bim-context-menu>
+      <bim-button class="transparent" @click=${onLoadIfc} label="Load IFC"></bim-button>
+      <bim-button class="transparent" @click=${onLoadFrag} label="Load FRAG"></bim-button>
+    </bim-context-menu>
+  </bim-button>`;
 };
 
 export default function LoadIfcButton({ components }: { components: OBC.Components }) {
